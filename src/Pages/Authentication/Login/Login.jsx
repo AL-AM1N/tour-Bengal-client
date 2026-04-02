670fe81738908ddf2419e14b148edd3ebe5fec27
MD. AL-AMIN <amin57.cse@gmail.com>
2026-04-02 23:55:57 +0600

    Get input data from login form using react hook form

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,32 +1,60 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="hero bg-base-200">
       <div className="hero-content flex-col">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login</h1>
+          <h1 className="text-5xl font-bold">Please Login</h1>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Username</label>
-              <input
-                className="input"
-                placeholder="Username"
-                
-              />
-              <label className="label">Password</label>
-              <input
-                type="password"
-                className="input"
-                placeholder="Password"
-                onChange={(e) => setPass(e.target.value)}
-              />
-              <button className="btn btn-neutral mt-4">
-                Login
-              </button>
-            </fieldset>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  {...register("email", { required: true })}
+                  placeholder="Enter your email"
+                />
+
+                {errors.email?.type === "required" && (
+                  <p className="text-red-500">Email required</p>
+                )}
+
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  {...register("password", { required: true, minLength: 6 })}
+                  placeholder="Password"
+                />
+
+                {/* handleding errors */}
+                {errors.password?.type === "required" && (
+                  <p className="text-red-500">Password required</p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p className="text-red-500">Password must be 6 charecters</p>
+                )}
+                <p>
+                  Create account?{" "}
+                  <span className="font-bold underline">Register</span>
+                </p>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
--- src/Pages/Root/AuthLayout.jsx
+++ src/Pages/Root/AuthLayout.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import AuthAnimation from "../../components/AuthAnimation/AuthAnimation";
 
 const AuthLayout = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <AuthAnimation></AuthAnimation>
-        <div>
-          <Outlet></Outlet>
+    <div>
+      <div>
+        <Link to="/">
+          <h1 className="font-serif font-bold text-4xl">
+            Tour
+            <span className="text-transparent bg-clip-text bg-linear-to-r from-orange-500 to-yellow-500">
+              Bengal
+            </span>
+          </h1>
+        </Link>
+      </div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="flex-1">
+            <AuthAnimation></AuthAnimation>
+          </div>
+          <div className="flex-1">
+            <Outlet></Outlet>
+          </div>
         </div>
       </div>
     </div>
